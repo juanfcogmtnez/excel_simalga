a516a8c6f15a5bd315451e546cbfc3ff56bd1c2c
juanfcogmtnez <juanfcogmtnez@gmail.com>
2021-11-10 16:48:34 +0100

    202111101648

---
+++ __init__.py
@@ -0,0 +1,4 @@
+# Copyright 2021 Juan Francisco García
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+from . import import_espacios
---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2020 Juan Francisco García
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Excel Import/Export/Report Simalga",
+    "version": "14.0.1.0.0",
+    "author": "Juan Fco García",
+    "license": "AGPL-3",
+    "category": "Tools",
+    "depends": ["excel_import_export","conf2"],
+    "data": [
+        "import_espacios/actions.xml",
+        "import_espacios/templates.xml",
+    ],
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": ["juanfcogmtnez"],
+}
